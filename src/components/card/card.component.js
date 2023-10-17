2d663b04d51f83b7d097c94ece2bc5dbb48d10ff
Animesh Kumar <animesh.kumar7717@gmail.com>
2023-10-17 10:31:20 +0530

    monster application with class component

---
+++ src/components/card/card.component.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+import './card.style.css'
+
+class Card extends Component {
+    render() {
+        const { name, id, email } = this.props.monsters;
+            return(
+                <div className="card-container" key={id} >
+                    <img alt={`monster: ${name}`} 
+                    src={`https://robohash.org/${id}`}
+                    />
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+            )
+    }
+}
+
+export default Card;
---
+++ src/components/search-box/search-box.component.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+import './search-box.style.css'
+
+class SearchBox extends Component {
+    render() {
+        const { onChangeHandler, placeholder, className } = this.props
+        return (
+            <div>
+                <input className={className} type='search' placeholder={placeholder} onChange={onChangeHandler} />
+            </div>
+        )
+    }
+}
+
+export default SearchBox
