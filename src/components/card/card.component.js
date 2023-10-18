5987de445d159a59802af623a396c83eab93ff92
Animesh Kumar <animesh.kumar7717@gmail.com>
2023-10-18 10:42:21 +0530

    react/function-component

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -1,20 +1,30 @@
-import { Component } from "react";
+// import { Component } from "react";
 
 import './card.style.css'
 
-class Card extends Component {
-    render() {
-        const { name, id, email } = this.props.monsters;
-            return(
-                <div className="card-container" key={id} >
-                    <img alt={`monster: ${name}`} 
-                    src={`https://robohash.org/${id}`}
-                    />
-                    <h2>{name}</h2>
-                    <p>{email}</p>
-                </div>
-            )
-    }
-}
+// class Card extends Component {
+//     render() {
+//         const { name, id, email } = this.props.monsters;
+//             return(
+//                 <div className="card-container" key={id} >
+//                     <img alt={`monster: ${name}`} 
+//                     src={`https://robohash.org/${id}`}
+//                     />
+//                     <h2>{name}</h2>
+//                     <p>{email}</p>
+//                 </div>
+//             )
+//     }
+// }
+
+const Card = ({ monsters: { name, id, email } }) => (
+    <div className="card-container" key={id} >
+        <img alt={`monster: ${name}`} 
+        src={`https://robohash.org/${id}`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+    </div>
+)
 
 export default Card;
